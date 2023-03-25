a315f163bc77d553515e7cc96c4653bc1be59287
yw88075 <126597268+yw88075@users.noreply.github.com>
2023-03-25 21:29:07 +0800

    Add files via upload

--- dr/js/酷奇MV.js
+++ dr/js/酷奇MV.js
@@ -3,10 +3,10 @@ var rule = {
     host:'https://www.kuqimv.com',
     homeUrl:'/play/',
     url:'/play/fyclass_fypage.html',
-    searchUrl:'/search.php?key=**',
+    searchUrl:'/search.php?key=**&pages=fypage',
     searchable:2,
     quickSearch:0,
-    class_parse:'.m_bor li;a&&Text;a&&href;/play/(\\d+)_1.html',
+    class_parse:'.main:eq(1)&&li;a&&Text;a&&href;.*/(\\d+)_1.html',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -16,7 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.mv_list li;.name&&Text;.pic img&&src;.singer&&Text;a&&href',
+    一级:'js:var d=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);let list=pdfa(html,".mv_list&&li");list.forEach((it)=>{d.push({title:pdfh(it,".name&&Text"),desc:"🎤"+pdfh(it,".singer&&Text")+"｜"+pdfh(it,".time&&Text"),pic_url:pd(it,"img&&src"),url:pd(it,"a&&href"),})});setResult(d);',
     二级:'*',
-    搜索:'.play_xg li;*;*;*;*',
+    搜索:'.video_list&&li;a&&title;;.singer&&Text;a&&href;.t_03:eq(1)&&Text',
 }
